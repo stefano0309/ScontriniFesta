1c49729b59ad7dabd5339dba80630af3d40bf37f
sdutto-3981 <s.dutto.3981@vallauri.edu>
2026-08-26 16:01:52 +0200

    newStructure

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'it.tabby.app',
+  appName: 'Tabby',
+  webDir: 'www'
+};
+
+export default config;
