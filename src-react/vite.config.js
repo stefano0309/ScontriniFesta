7465a597cf4c71be10e6fb8bc8b261152022df05
Stefano Dutto <117385811+stefano0309@users.noreply.github.com>
2026-09-03 00:16:23 +0200

    Update Vite config for repository base path

--- src-react/vite.config.js
+++ src-react/vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Sostituisci 'nome-repository' con il nome esatto della tua repo su GitHub
+  base: process.env.NODE_ENV === 'production' ? '/nome-repository/' : '/',
   build: {
-    // Diciamo a Vite di mandare il codice compilato nella cartella di Capacitor
-    outDir: '../www', 
-    emptyOutDir: true
+    outDir: '../www', // o 'www' a seconda della tua struttura
+    emptyOutDir: true,
   }
 })
